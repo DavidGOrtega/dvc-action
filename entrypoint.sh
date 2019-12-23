1d3ac5085dcfb2684aa78d956d6aed2ad799ba59
davidgortega <g.ortega.david@gmail.com>
2019-12-23 16:40:37 +0100

    skip ci info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 set -e
-# printenv
 
-COMMIT_FILTER="dvc repro"
+COMMIT_FILTER="[ci skip]"
 # Skip if commit filter
 readonly local last_commit_log=$(git log -1)
 readonly local filter_count=$(echo "$last_commit_log" | grep -c "$COMMIT_FILTER" )
-echo "Check skip last commit $last_commit_log"
 if ! [[ "$filter_count" -eq 0 ]]; then
-  echo "Last commit log \"$last_commit_log\" contains \"$COMMIT_FILTER\", skipping"
+  echo "[ci skip] found!"
   exit 0 # exit 78 # 78 is neutral github code 
 fi
 
@@ -25,7 +23,7 @@ if ! git diff-index --quiet HEAD --; then
     
     git config --local user.email "action@github.com"
     git config --local user.name "GitHub Action"
-    git commit -m "${COMMIT_FILTER}; Leave me to ci skip!" -a
+    git commit -m "dvc repro ${COMMIT_FILTER}" -a
     git remote add github "https://$GITHUB_ACTOR:$github_token@github.com/$GITHUB_REPOSITORY.git"
     git push github HEAD:${GITHUB_REF}
 
