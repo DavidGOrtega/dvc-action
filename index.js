d36bb9772f20ec229bb61f3afd233ccc87b42620
DavidGOrtega <g.ortega.david@gmail.com>
2019-12-31 02:24:58 +0000

    skjsd

--- index.js
+++ index.js
@@ -379,7 +379,6 @@ const run_repro = async () => {
 const install_dependencies = async () => {
   console.log('installing dvc...')
   await exe('pip install dvc');
-  await exe('apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev');
 }
 
 const vega2md = async (name, vega_data) => {
