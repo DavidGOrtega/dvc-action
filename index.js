dfc26c2c525d0b9182a0e1ad91b348a016cc386c
DavidGOrtega <g.ortega.david@gmail.com>
2019-12-31 02:40:57 +0000

    skjsd

--- index.js
+++ index.js
@@ -379,6 +379,7 @@ const run_repro = async () => {
 const install_dependencies = async () => {
   console.log('installing dvc...')
   await exe('pip install dvc');
+  await exe('sudo npm i -g canvas --unsafe-perm=true --allow-root');
 }
 
 const vega2md = async (name, vega_data) => {
