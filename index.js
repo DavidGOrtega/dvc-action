8d9db5353792ad2e57544f3777dbcc654fba41db
DavidGOrtega <g.ortega.david@gmail.com>
2019-12-31 00:34:58 +0000

    canvas

--- index.js
+++ index.js
@@ -379,6 +379,7 @@ const run_repro = async () => {
 const install_dependencies = async () => {
   console.log('installing dvc...')
   await exe('pip install dvc');
+  await exe('npm install -g canvas');
 }
 
 const vega2md = async (name, vega_data) => {
