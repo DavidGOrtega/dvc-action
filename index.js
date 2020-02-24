dc5dfc3c6c78fe3cc53b2271d6202fa0450098f6
DavidGOrtega <g.ortega.david@gmail.com>
2020-02-24 15:30:57 +0000

    back

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
+
+const { exec } = require('./src/utils')
 const DVC = require('./src/Dvc')
 const CI = require('./src/CI')
 
