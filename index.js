7cb404e1dedb88ce9c4f97a913475b01baf06599
DavidGOrtega <g.ortega.david@gmail.com>
2020-02-24 15:15:13 +0000

    back

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const create_check_dvc_report = async (opts) => {
 const run = async () => {
   const is_pr = GITHUB_EVENT_NAME === 'pull_request';
   const ref = is_pr ? GITHUB_HEAD_REF : GITHUB_REF;
-  const head_sha = is_pr ? (await exec(`git rev-parse ${}ref`, { throw_err: false })).replace(/(\r\n|\n|\r)/gm, "") :
+  const head_sha = is_pr ? (await exec(`git rev-parse ${ref}`, { throw_err: false })).replace(/(\r\n|\n|\r)/gm, "") :
     GITHUB_SHA;
   const [owner, repo] = GITHUB_REPOSITORY.split('/');
   const user_email = 'action@github.com';
